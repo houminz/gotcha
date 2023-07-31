5f5c3ed08ea61312abd23d2ccfaabd2b0e8238ce
Houmin Wei <weihoumin@gmail.com>
2023-07-31 09:49:35 +0800

    feat: add consistent hash, to be confirmed

---
+++ hash/pkg/consistent-hash/consistent-hash.go
@@ -0,0 +1,25 @@
+package consistent-hash
+
+import (
+	"hash/crc32"
+	
+)
+type ConsistentHash struct {
+
+}
+
+func New() *ConsistentHash {
+	return &ConsistentHash{
+
+	}
+}
+
+func (c *ConsistentHash) Add(element string) {
+
+}
+
+func (c *ConsistentHash) Remove(element string) {
+
+}
+
+func (c *ConsistentHash) Get()
